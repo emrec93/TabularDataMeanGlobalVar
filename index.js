6af0de7ef4a4bdb6487660a43c4016b460e4c2fb
Emre C <41086511+emrec93@users.noreply.github.com>
2020-09-23 15:20:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+// Creating The Chart with Chart.js
+async function createChart() {
+    const data = await getData();
+    const ctx = document.getElementById('chart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.xs,
+            datasets: [{
+                label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in °C From 1880 to 2019',
+                data: data.ys,
+                fill: false,
+                backgroundColor: 'rgba(27, 47, 51, 0.9)',
+                borderColor: 'rgba(84, 199, 222, 0.9)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false,
+                        callback: function (value, index, values) {
+                            return value + "°C";
+                        }
+                    }
+                }]
+            }
+        }
+    });
+}
+
+// Data from: https://data.giss.nasa.gov/gistemp/
+// Mean from: https://earthobservatory.nasa.gov/
+
+// Get Data
+async function getData() {
+
+    const xs = [];
+    const ys = [];
+
+
+    // Fetch data and transform it into text
+    const response = await fetch('zonalAnnualMeans.csv');
+    const data = await response.text();
+
+    // Tidy data by selecting only year and temp
+    const table = data.split('\n').slice(1);
+    table.forEach(row => {
+
+        const columns = row.split(',');
+        const year = columns[0];
+        xs.push(year);
+        const temp = columns[1];
+        ys.push(parseFloat(temp) + 14);
+
+        // log it to console to see it
+        console.log(year, temp);
+    });
+
+    return { xs, ys };
+}
+
+//Date for the footer
+let d = new Date();
+document.getElementById("footer-date").textContent = d.getFullYear();
